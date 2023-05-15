01877146bafc45b2080917dd507577b5a748aa35
FalejevV <v.o.v.a@hotmail.com>
2023-05-15 14:09:39 +0300

    Loaded stage model

--- src/components/Lights/Stage/Stage.tsx
+++ src/components/Lights/Stage/Stage.tsx
@@ -1,54 +1,39 @@
 import { RootState, useAppSelector } from "@/store/store";
 import { MeshReflectorMaterial, OrbitControls, PerspectiveCamera, useHelper } from "@react-three/drei";
-import { Canvas } from "@react-three/fiber";
-import { Suspense, useRef } from "react";
+import { Canvas, useLoader } from "@react-three/fiber";
+import { Suspense } from "react";
+import {StageModel} from "./StageModel";
+import styled from "styled-components";
 
-
+const StageContainer = styled.div`
+    width:100vw;
+    height:100vh;
+`
 function StageObject(){
     // const light:any = useRef();
     // useHelper(light, SpotLightHelper, 'red')
-
     const faderSelector = useAppSelector((state:RootState) => state.faders);
     return <>
-        <OrbitControls />
         <PerspectiveCamera makeDefault fov={100} position={[0,0,4]} />
         {
             <color args={[0.01,0.01,0.01]} attach="background" />
         }
-        <spotLight
-                color={[1,0.25,0.7]}
-                intensity={faderSelector[0]}
-                angle={0.6}
-                penumbra={0.5}
-                castShadow
-                position={[3,3,4]}
-                distance={8}
-             />
-        <spotLight
-                color={[0.1,0.1,0.7]}
-                intensity={faderSelector[1]}
-                angle={0.6}
-                penumbra={0.5}
-                castShadow
-                position={[-2,2,2]}
-                distance={8}
-             />
-        
-        <mesh castShadow receiveShadow position={[0,1,0]}>
-            <boxGeometry args={[2,2,2]}/>
-            <meshBasicMaterial color={"#3b3b3b"}/>
-            <MeshReflectorMaterial mirror={0} />
+        <mesh position={[-0.3,-2,-13]} rotation={[0.15,0,0]}>
+            <StageModel />
         </mesh>
+        <spotLight intensity={0.5} position={[0,0,1]} />
     </>
 }
 
 function Stage(){
     return(
-        <Suspense fallback={null}>
-            <Canvas shadows>
-                <StageObject />
-            </Canvas>
-        </Suspense>
+        <StageContainer>
+            <Suspense fallback={null}>
+                <Canvas shadows>
+                    <StageObject />
+                </Canvas>
+            </Suspense>
+        </StageContainer>
     )
 }
 
